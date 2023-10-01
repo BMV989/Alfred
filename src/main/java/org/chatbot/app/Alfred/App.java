62ee04344361a272fa2e5139fe706222c1cc481c
Maxim <bmv989@skiff.com>
2023-10-01 21:43:26 +0500

    unit tests for help, default cases

--- src/main/java/org/chatbot/app/Alfred/App.java
+++ src/main/java/org/chatbot/app/Alfred/App.java
@@ -5,42 +5,33 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class App {
-  public static void main(String[] args) {
-    String user = System.getProperty("user.name");
-    String greetings = String.format("Hello, %s!", user);
-    System.out.println(greetings);
-    System.out.println("How can I help you?");
-    Scanner scanner = new Scanner(System.in);
-    session: while (true) {
-      System.out.print("> ");
-      String userInput = scanner.nextLine();
-      // TODO: come up with the idea of the chatbot
-      switch (userInput) {
-        case "/exit":
-          break session;
-        case "/help":
-          System.out.println("List of commands:");
-          System.out.println("/exit --- quit Alfred");
-          System.out.println("/help --- show help");
-          System.out.println("/flip --- flip coin");
-          System.out.println("/time --- show date and time");
-          break;
-        case "/flip":
-          System.out.print("Flipping coin... ");
-          System.out.println(Math.random() > 0.5 ? "Heads!" : "Tails!");
-          break;
-        case "":
-          break;
-        case "/time":
-          DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/uu HH:mm:ss");
-          LocalDateTime now = LocalDateTime.now();
-          System.out.println(dtf.format(now));
-          break;
-        default:
-          System.out.println("There's no such command!");
-          System.out.println("/help for help");
-          break;
-      }
+
+    public static void main(String[] args) {
+        System.out.printf("Hello, %s!%nHow can I help you?%n", System.getProperty("user.name"));
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        while (!exit) {
+            System.out.print("> ");
+            String userInput = scanner.nextLine();
+            switch (userInput) {
+                case "/exit" -> exit = true;
+                case "/help" -> System.out.println("""
+                    List of commands:
+                    /exit --- quit Alfred
+                    /help --- show help
+                    /flip --- flip coin
+                    /time --- show date and time""");
+                case "/flip" -> System.out.printf("Flipping coin... %s!%n",
+                    Math.random() > 0.5 ? "Heads" : "Tails");
+                case "" -> {
+                }
+                case "/time" -> {
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/uu HH:mm:ss");
+                    LocalDateTime now = LocalDateTime.now();
+                    System.out.println(dtf.format(now));
+                }
+                default -> System.out.println("There's no such command!\n/help for help");
+            }
+        }
     }
-  }
 }
