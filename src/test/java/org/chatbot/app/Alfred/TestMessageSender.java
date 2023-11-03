1b8928761c90e75bf738dfff78570fb9deb1519b
Maxim <bmv989@skiff.com>
2023-11-03 18:46:50 +0500

    fix race condition

---
+++ src/test/java/org/chatbot/app/Alfred/TestMessageSender.java
@@ -0,0 +1,16 @@
+package org.chatbot.app.Alfred;
+
+import org.chatbot.app.Alfred.telegram.MessageSender;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+public class TestMessageSender implements MessageSender {
+    private SendMessage msg;
+
+    @Override
+    public void sendMsg(Long chatId, String message) {
+        msg = SendMessage.builder().chatId(chatId).text(message).build();
+    }
+    public SendMessage getMsg() {
+        return msg;
+    }
+}
